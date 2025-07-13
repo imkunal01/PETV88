30165308453b3ac87514481139b18eb7217da067
imkunal01 <142741387+imkunal01@users.noreply.github.com>
2025-07-14 00:45:20 +0530

    Initialised project work for McDonald's clone application with a structured frontend and backend setup.

---
+++ frontend/src/components/header/navbar/nav.jsx
@@ -0,0 +1,14 @@
+import './nav.css'
+import logo from '../../../assets/mcdlogo.svg'
+function Nav() {
+  return (
+  <>
+    <div className="mcdlogo">
+        <img src={logo} alt="McDLogo" />
+        
+    </div>
+  </>
+  )
+}
+
+export default Nav
